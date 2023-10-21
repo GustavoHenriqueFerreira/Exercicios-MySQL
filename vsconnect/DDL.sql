cae9ecdfe98143bd2c02dbbe9c8117c35ab376a1
Gustavo-Henrique-Ferreira-Alves <gustavohferreiraalves@gmail.com>
2023-10-21 01:09:21 -0300

    feat: DQL (Data Query Language) do projeto VSConnect

--- vsconnect/DDL.sql
+++ vsconnect/DDL.sql
@@ -1,57 +1,57 @@
-CREATE DATABASE dbvsconnect;
+CREATE DATABASE bd_vsconnect;
 
-USE dbvsconnect;
+USE bd_vsconnect;
 
-CREATE TABLE tbtechs(
+CREATE TABLE tb_techs(
 	id_tech BINARY(16) NOT NULL,
     nome VARCHAR(50) NOT NULL UNIQUE,
     PRIMARY KEY(id_tech)
 );
 
-CREATE TABLE tbusuarios(
-	id_usuario BINARY(16) NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    endereco VARCHAR(255),
-    cep VARCHAR(9) NOT NULL,
-    tipo_usuario TINYINT NOT NULL,
-    PRIMARY KEY(id_usuario)
+CREATE TABLE tb_usuarios(
+	id_usuario	BINARY(16) NOT NULL,
+	nome VARCHAR(255)NOT NULL,
+	email VARCHAR(255)NOT NULL UNIQUE,
+	senha VARCHAR(255)NOT NULL,
+	endereco VARCHAR(255),
+	cep VARCHAR(255),
+	tipo_usuario TINYINT NOT NULL,
+	PRIMARY KEY (id_usuario)
 );
 
-CREATE TABLE tbdevtech(
-	id_dev BINARY(16) NOT NULL,
-    id_tech BINARY(16) NOT NULL,
-	FOREIGN KEY(id_tech) REFERENCES tbtechs(id_tech),
-    FOREIGN KEY(id_dev) REFERENCES tbusuarios(id_usuario)
+CREATE TABLE tb_dev_tech(
+	id_dev	BINARY(16) NOT NULL,
+	id_tech	BINARY(16) NOT NULL,
+	FOREIGN KEY(id_dev) REFERENCES tb_usuarios(id_usuario),
+	FOREIGN KEY(id_tech) REFERENCES tb_techs(id_tech)
 );
 
-CREATE TABLE tbservicos(
+CREATE TABLE tb_servicos(
 	id_servico BINARY(16) NOT NULL,
     titulo VARCHAR(255) NOT NULL,
     descricao TEXT NOT NULL,
-	proposta DECIMAL(10, 2) NOT NULL,
+    proposta DECIMAL(10, 2) NOT NULL,
     status_servico VARCHAR(50) NOT NULL,
-    id_cliente BINARY(16) NOT NULL,
+    id_cliente BINARY(16),
     id_dev BINARY(16),
-    PRIMARY KEY(id_servico),
-    FOREIGN KEY(id_cliente) REFERENCES tbusuarios(id_usuario),
-    FOREIGN KEY(id_dev) REFERENCES tBusuarios(id_usuario)
-);
-
-CREATE TABLE tbcotacao(
+    PRIMARY KEY (id_servico),
+    FOREIGN KEY(id_cliente) REFERENCES tb_usuarios(id_usuario),
+    FOREIGN KEY(id_dev) REFERENCES tb_usuarios(id_usuario)
+);  
+  
+CREATE TABLE tb_cotacao(
 	id_cotacao BINARY(16) NOT NULL,
-    valor DECIMAL(10, 2) NOT NULL,
-	id_dev BINARY(16) NOT NULL,
+    valor DECIMAL (10, 2)NOT NULL,
+    id_dev BINARY (16) NOT NULL,
     id_servico BINARY(16) NOT NULL,
-    PRIMARY KEY(id_cotacao),
-    FOREIGN KEY(id_dev) REFERENCES tBusuarios(id_usuario),
-    FOREIGN KEY(id_servico) REFERENCES tbservicos(id_servico)
-);
+    PRIMARY KEY (id_cotacao),
+	FOREIGN KEY(id_dev) REFERENCES tb_usuarios(id_usuario),
+    FOREIGN KEY(id_servico) REFERENCES tb_servicos(id_servico)
+);  
 
-CREATE TABLE tbtechservico(
+CREATE TABLE tb_tech_servico(
 	id_tech BINARY(16) NOT NULL,
     id_servico BINARY(16) NOT NULL,
-    FOREIGN KEY(id_tech) REFERENCES tbtechs(id_tech),
-    FOREIGN KEY(id_servico) REFERENCES tbservicos(id_servico)
+    FOREIGN KEY(id_tech) REFERENCES tb_techs(id_tech),
+    FOREIGN KEY(id_servico) REFERENCES tb_servicos(id_servico)
 );
--- vsconnect/DML.sql
+++ vsconnect/DML.sql
@@ -1,31 +1,46 @@
-USE dbvsconnect;
+USE bd_vsconnect;
 
 #TECHS
-INSERT INTO tbtechs
+INSERT INTO tb_techs
 VALUES (UUID_TO_BIN(UUID()), "React"), (UUID_TO_BIN(UUID()), "HTML"), (UUID_TO_BIN(UUID()), "Java");
 
-SELECT BIN_TO_UUID(id_tech), nome FROM tbtechs;
+SELECT BIN_TO_UUID(id_tech), nome FROM tb_techs;
 
 #USUARIOS
-INSERT INTO tbusuarios
+INSERT INTO tb_usuarios
 VALUES (UUID_TO_BIN(UUID()), "Odirlei", "odirlei@gmail.com", "senai", "Rua Niteroi", "12345-678", 0), (UUID_TO_BIN(UUID()), "Alexia", "alexia@gmail.com", "senai", "Rua Niteroi", "12345-678", 0), (UUID_TO_BIN(UUID()), "Thiago", "thiago@gmail.com", "senai", "Rua Niteroi", "12345-678", 0);
 
-DELETE FROM tbusuarios WHERE id_usuario = UUID_TO_BIN("8c8d5817-6edc-11ee-b760-b445067b85b0");
+DELETE FROM tb_usuarios WHERE id_usuario = UUID_TO_BIN("8c8d5817-6edc-11ee-b760-b445067b85b0");
 
-UPDATE tbusuarios SET tipo_usuario = 1 WHERE id_usuario = UUID_TO_BIN("57d8bcbe-6edd-11ee-b760-b445067b85b0");
+UPDATE tb_usuarios SET tipo_usuario = 1 WHERE id_usuario = UUID_TO_BIN("c63821aa-6fc4-11ee-b960-00e04c9f5652");
 
-SELECT BIN_TO_UUID(id_tech), nome FROM tbtechs;
-SELECT BIN_TO_UUID(id_usuario), nome, email, senha, endereco, cep, tipo_usuario FROM tbusuarios;
+SELECT BIN_TO_UUID(id_tech), nome FROM tb_techs;
+SELECT BIN_TO_UUID(id_usuario), nome, email, senha, endereco, cep, tipo_usuario FROM tb_usuarios;
 
 #TABELA INTERMEDIARIA DEV/TECH
-INSERT INTO tbdevtech
-VALUES (UUID_TO_BIN("1fd09926-6ede-11ee-b760-b445067b85b0"), UUID_TO_BIN("8c8be5b5-6edc-11ee-b760-b445067b85b0")), (UUID_TO_BIN("57d8bcbe-6edd-11ee-b760-b445067b85b0"), UUID_TO_BIN("8c8b96e6-6edc-11ee-b760-b445067b85b0"));
+INSERT INTO tb_dev_tech
+VALUES (UUID_TO_BIN("c6381e3d-6fc4-11ee-b960-00e04c9f5652"), UUID_TO_BIN("a14bca81-6fc4-11ee-b960-00e04c9f5652")), (UUID_TO_BIN("c63821aa-6fc4-11ee-b960-00e04c9f5652"), UUID_TO_BIN("a14bc4a2-6fc4-11ee-b960-00e04c9f5652"));
 
-SELECT BIN_TO_UUID(id_dev), BIN_TO_UUID(id_tech) FROM tbdevtech;
+SELECT BIN_TO_UUID(id_dev), BIN_TO_UUID(id_tech) FROM tb_dev_tech;
 
-#SERVIÇOS
-select * from tbservicos;
-INSERT INTO tbservicos VALUES
-    (UUID_TO_BIN(UUID()), "Dashboard", "Desenvolver uma dashboard com informações importantes do nosso controle de vendas.", "3000", "Concluído", UUID_TO_BIN("1fd09926-6ede-11ee-b760-b445067b85b0")),
-    (UUID_TO_BIN(UUID()), "Desenvolvimento de site institucional - Gateway de Pagamento / Fintech", "Desenvolver um site responsivo que seja utilizado como uma plataforma de apresentação do nosso gateway de pagamento. O objetivo principal deste projeto é criar um site atraente e informativo, que demonstre as funcionalidades e benefícios do nosso gateway de pagamento para potenciais clientes.", "1300", "Em andamento", UUID_TO_BIN("1fd09926-6ede-11ee-b760-b445067b85b0")),
-    (UUID_TO_BIN(UUID()), "Preciso da estrutura de uma HomePage", "Preciso fazer uma tela somente estruturada em HTML para minha empresa.", "1000", "Pendente", UUID_TO_BIN("1fd09926-6ede-11ee-b760-b445067b85b0"));
+#TABELA DE SERVICOS
+INSERT INTO tb_servicos VALUES
+	(UUID_TO_BIN(UUID()), "Dashboard", "Desenvolver uma dashboard com informações importantes do nosso controle de vendas.", "3000", "Concluído", UUID_TO_BIN("c63822d7-6fc4-11ee-b960-00e04c9f5652"), NULL),
+	(UUID_TO_BIN(UUID()), "Desenvolvimento de site institucional - Gateway de Pagamento / Fintech", "Desenvolver um site responsivo que seja utilizado como uma plataforma de apresentação do nosso gateway de pagamento. O objetivo principal deste projeto é criar um site atraente e informativo, que demonstre as funcionalidades e benefícios do nosso gateway de pagamento para potenciais clientes.", "1300", "Em andamento", UUID_TO_BIN("c63822d7-6fc4-11ee-b960-00e04c9f5652"), NULL),
+	(UUID_TO_BIN(UUID()), "Preciso da estrutura de uma HomePage", "Preciso fazer uma tela somente estruturada em HTML para minha empresa.", "1000", "Pendente", UUID_TO_BIN("c63822d7-6fc4-11ee-b960-00e04c9f5652"), NULL);
+    
+SELECT BIN_TO_UUID(id_servico), titulo FROM tb_servicos;
+
+#TABELA DE COTACAO
+INSERT INTO tb_cotacao VALUES(
+	UUID_TO_BIN(UUID()),
+    10000.00,
+    UUID_TO_BIN("c6381e3d-6fc4-11ee-b960-00e04c9f5652"),
+    UUID_TO_BIN("b3192019-6fc5-11ee-b960-00e04c9f5652")
+);
+
+SELECT BIN_TO_UUID(id_cotacao), valor, BIN_TO_UUID(id_dev), BIN_TO_UUID(id_servico) FROM tb_cotacao;
+
+#TESTE DE COMO FUNCIONA A TABELA INTERMEDIARIA
+INSERT INTO tb_dev_tech
+VALUES (UUID_TO_BIN("c63821aa-6fc4-11ee-b960-00e04c9f5652"), UUID_TO_BIN("a14bca81-6fc4-11ee-b960-00e04c9f5652")), (UUID_TO_BIN("c6381e3d-6fc4-11ee-b960-00e04c9f5652"), UUID_TO_BIN("a14bc4a2-6fc4-11ee-b960-00e04c9f5652"));
